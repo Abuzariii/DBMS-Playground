51226f306f5b54b2afb0941783f5f3a752f1c5f3
Muhammad Abuar <muhammadabuzar010@gmail.com>
2023-06-09 21:01:37 +0500

    PostgreSQL Day 1 pt 2

--- PostgreSQL/Operations on user_details table.sql
+++ PostgreSQL/Operations on user_details table.sql
@@ -30,9 +30,27 @@ SELECT username FROM user_details OFFSET 91 LIMIT 1;
 
 -- Now change that value
 UPDATE user_details
-SET username = 'bellllaaaaaa'
-WHERE username = 'ross99';
+SET username = 'rogers1003'
+WHERE username = 'rogers163';
 
+-- SQL doesn't have any inherent order, you must use an indexing column or the edited column goes at bottom of the table
+-- hence we are going to order by user id in ascending order
+SELECT * FROM user_details ORDER BY user_id asc;
+
+
+-- Select all female users
+select * from user_details where gender='Female' order by user_id asc;
+
+
+-- Select all users with username length equal to or more than 10
+select * from user_details where length(username)>=10 order by user_id asc;
+
+
+-- Select all usernames which contain john in them
+select * from user_details where username like '%john%' order by user_id asc;
+
+-- Join multiple conditions using and
+select * from user_details where username like '%john%' and length(username)>5 and username like '%24%' order by user_id asc;
 
 
 
